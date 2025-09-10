2d2c5b446676963c296a3905fe759e1c6d9d6de7
Rawen Tozri <raweentozri@gmail.com>
2025-09-10 10:36:57 +0200

    Daily update

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -17,3 +17,65 @@ export const create = async (req, res) => {
         res.status(500).json({ errorMessage: error.message });
     }
 };
+export const getAllUsers = async (req, res) => {
+    try {
+        const users = await User.find(); // récupère tous les utilisateurs
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json({ errorMessage: error.message });
+    }
+};
+// Get user by ID (sans _id et __v)
+export const getUserById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id).select("name email adresse -_id");
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ errorMessage: error.message });
+  }
+};
+
+// Update user by ID
+export const updateUser = async (req, res) => {
+  try {
+    const { id } = req.params;          // récupère l'id de l'URL
+    const updateData = req.body;         // données à mettre à jour
+
+    // findByIdAndUpdate(id, update, options)
+    const updatedUser = await User.findByIdAndUpdate(
+      id,
+      updateData,
+      { new: true, runValidators: true } // renvoie le document mis à jour
+    );
+
+    if (!updatedUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    res.status(500).json({ errorMessage: error.message });
+  }
+};
+// Supprimer un utilisateur par ID
+export const deleteUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const deletedUser = await User.findByIdAndDelete(id);
+
+    if (!deletedUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json({ message: "User deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ errorMessage: error.message });
+  }
+};
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,9 +1,15 @@
 import express from "express"
-import {create } from "../controller/userController.js"
+import { create, getAllUsers ,getUserById, updateUser,deleteUser} from "../controller/userController.js"; 
 
 const route = express.Router();
 
 route.post("/user",create);
+route.get("/user", getAllUsers);
+route.get("/user/:id",getUserById);
+route.put("/user/:id", updateUser);
+route.delete("/user/:id", deleteUser);
 
 export default route ;
+
+
 
