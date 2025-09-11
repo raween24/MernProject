b9bfb54c585bfc0270795a42c9a77bcde14baca8
Rawen Tozri <raweentozri@gmail.com>
2025-09-11 11:18:50 +0200

    Daily update

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,28 @@
 import React from "react";
 import User from "./getuser/User";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route , Navigate } from "react-router-dom";
 import AddUser from "./adduser/adduser";
 import UpdateUser from "./updateuser/updateuser";
 import Login from "./login/Login"; 
 function App() {
-  const token = localStorage.getItem("token"); // vérifier si connecté
+  const token = localStorage.getItem("token"); // Vérifie si l’utilisateur est connecté
   return (
     <Router>
       <div className="App">
         <header className="App-header">
-          <h1></h1>
+          <h1>User Management App</h1>
         </header>
 
         <Routes>
-          <Route path="/" element={<User />} />
-           <Route path="/add" element={<AddUser />} />
-          <Route path="/update/:id" element={<UpdateUser />} />
+          {/* Login accessible sans être connecté */}
+          <Route path="/login" element={<Login />} />
+
+          {/* Si connecté → affiche User, sinon → redirige vers Login */}
+          <Route path="/" element={token ? <User /> : <Navigate to="/login" />} />
+
+          {/* Protégées par token */}
+          <Route path="/add" element={token ? <AddUser /> : <Navigate to="/login" />} />
+          <Route path="/update/:id" element={token ? <UpdateUser /> : <Navigate to="/login" />} />
         </Routes>
       </div>
     </Router>
